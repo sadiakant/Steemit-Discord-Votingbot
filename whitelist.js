b2a4c07265968719ea35e3609fa52518e5953358
Rishi Panthee <rishipanthee@hotmail.com>
2018-10-01 18:39:58 -0500

    Witelist error fixes for api

--- whitelist.js
+++ whitelist.js
@@ -3,14 +3,20 @@ var fs = require("fs")
 function addToWhitelist(user, message) {
     var whitelist = JSON.parse(fs.readFileSync("whitelist.json"));
     if (whitelist.includes(user)) {
-        message.channel.send("<@" + message.author.id + "> " + user + " is already in the whitelist.")
+        if (message != null) {
+            message.channel.send("<@" + message.author.id + "> " + user + " is already in the whitelist.")
+        }
     } else {
         whitelist.push(user)
         fs.writeFile('whitelist.json', JSON.stringify(whitelist, null, 2), function(err) {
             if (err == null) {
-                message.channel.send("<@" + message.author.id + "> Sucessfully added " + user + " to whitelist.")
-            } else {
-                message.channel.send("<@" + message.author.id + "> Error adding " + user + " to whitelist. Please try again")
+                if (message != null) {
+                    message.channel.send("<@" + message.author.id + "> Sucessfully added " + user + " to whitelist.")
+                } else {
+                    if (message != null) {
+                        message.channel.send("<@" + message.author.id + "> Error adding " + user + " to whitelist. Please try again")
+                    }
+                }
             }
         })
     }
@@ -23,15 +29,20 @@ function removeFromWhitelist(user, message) {
         if (aIndex > -1) {
             whitelist.splice(aIndex, 1);
             fs.writeFile('whitelist.json', JSON.stringify(whitelist, null, 2), function(err) {
+                if (message != null) {
                 if (err == null) {
-                    message.channel.send("<@" + message.author.id + "> Sucessfully remvoed " + user + " from whitelist.")
-                } else {
-                    message.channel.send("<@" + message.author.id + "> Error removing " + user + " from whitelist. Please try again")
+                    
+                        message.channel.send("<@" + message.author.id + "> Sucessfully remvoed " + user + " from whitelist.")
+                    } else {
+                        message.channel.send("<@" + message.author.id + "> Error removing " + user + " from whitelist. Please try again")
+                    }
                 }
             })
         }
     } else {
-        message.channel.send("<@" + message.author.id + "> " + user + " is not in the whitelist.")
+        if (message != null) {
+            message.channel.send("<@" + message.author.id + "> " + user + " is not in the whitelist.")
+        }
     }
 }
 
