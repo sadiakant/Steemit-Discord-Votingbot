dc9679ac417b872cd1bef613db2c907b8c12f1ba
Rishi Panthee <rishipanthee@hotmail.com>
2018-10-01 18:39:39 -0500

    Started API For Mobile App

--- api.js
+++ api.js
@@ -1,8 +1,7 @@
 var express = require('express');
 const bodyParser = require('body-parser');
 var fs = require("fs")
-
-
+var whitelist = require("./whitelist.js")
 var app = express();
 var router = express.Router()
 app.use(bodyParser.urlencoded({
@@ -12,18 +11,72 @@ app.use(bodyParser.json());
 
 app.use(router)
 
-router.get('', function (req, res) {
-    var a = JSON.parse(fs.readFileSync("config.json"))
-    delete a["privatePostingKey"]
-    delete a["privateActiveKey"]
-    res.json(a)
+var password = ""
+
+router.get('', function(req, res) {
+    if (req.headers.password == password) {
+        res.json({
+            "message": "Success"
+        })
+    } else {
+        res.json({
+            "message": "Incorrect Password"
+        })
+    }
 })
 
-function start()
-{
+router.get('/whitelist', function(req, res) {
+    if (req.headers.password == password) {
+        fs.readFile("whitelist.json", function(err, data) {
+            if (!err) {
+                var jsonWhitelist = JSON.parse(data)
+                res.json(jsonWhitelist)
+
+            }
+        })
+    } else {
+        res.json({
+            "message": "Incorrect Password"
+        })
+    }
+})
+
+router.post("/whitelist", function(req, res) {
+    if (req.headers.password == password) {
+
+        if (req.body.activity == "add") {
+            let user = req.body.user
+            whitelist.addToWhitelist(user.toLowerCase(), null)
+            res.json({
+                "message": "Added"
+            })
+        }
+
+        if (req.body.activity == "remove") {
+            let user = req.body.user
+            whitelist.removeFromWhitelist(user.toLowerCase(), null)
+            res.json({
+                "message": "Removed"
+            })
+        }
+    }
+
+})
+
+
+
+
+function start() {
     app.listen(8080)
+    setInterval(function() {
+        fs.readFile("whitelist.json", function(err, data) {
+            if (!err) {
+                password = data.apiPassword
+            }
+        })
+    }, 10 * 1000)
 }
 
 module.exports = {
-    start : start
+    start: start
 }
